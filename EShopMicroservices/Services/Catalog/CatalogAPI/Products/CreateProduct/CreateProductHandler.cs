da5c2d55213fa413d0cc92cdf1f001c8c6cd8491
Marcelo Bertinatto <marcelobertinatto@hotmail.com>
2024-08-06 22:31:30 -0300

    finalized Handler and initiating the usage of Carter lib in the building blocks project

--- EShopMicroservices/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ EShopMicroservices/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,17 +1,29 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Model;
 
 namespace CatalogAPI.Products.CreateProduct
 {
-    public record CreateProductCommand(string name, List<string>Category, 
-        string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+    public record CreateProductCommand(string Name, List<string>Category, 
+        string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command
+            , CancellationToken cancellationToken)
         {
-            // business logic to create a product
-            throw new NotImplementedException();
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            return new CreateProductResult(Guid.NewGuid());
         }
     }
 }
